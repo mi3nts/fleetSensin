374bd7c64531c25e2cc66ac4ecb457db7ff4a2db
Teamlary <lhw150030@utdallas.edu>
2021-04-12 19:18:10 -0500

    intial Code

---
+++ firmware/xu4_mqtt/runAll.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+sleep 60
+python3 nanoReader.py 0 &
+sleep 5
+python3 nanoReader.py 1 &
+sleep 5
+python3 GPSReader.py &
+sleep 5
+python3 ipReader.py
+
+# python3 centralNodeReaderNano.py &
+# sleep 10
+# cd odroidShow2 && python3 mintsShow2.py
